070a235c538f5403620653038cfce7eb539bccc2
Your Name <you@example.com>
2022-12-27 14:44:08 +0330

    https://leetcode.com/problems/running-sum-of-1d-array/

--- leetcode.py
+++ leetcode.py
@@ -1,5 +1,9 @@
-def runningSum(nums):
-    target = []
-    for i in range(len(nums)):
-        target.append(sum(nums[:i+1]))
-    return target
+# https://leetcode.com/problems/running-sum-of-1d-array/
+
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        target = []
+        for i in range(len(nums)):
+            target.append(sum(nums[:i+1]))
+        return target
+
