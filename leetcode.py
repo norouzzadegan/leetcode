d2212874576eab6983e0f254d190b8c2b330a70e
Your Name <you@example.com>
2022-12-27 14:44:28 +0330

    https://leetcode.com/problems/richest-customer-wealth/

--- leetcode.py
+++ leetcode.py
@@ -7,3 +7,8 @@ class Solution:
             target.append(sum(nums[:i+1]))
         return target
 
+# https://leetcode.com/problems/richest-customer-wealth/
+
+class Solution:
+    def maximumWealth(self, accounts: List[List[int]]) -> int:
+        return max([sum(i) for i in accounts])
